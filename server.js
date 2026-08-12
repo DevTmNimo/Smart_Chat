f6f58760c67b517017cd324240f433096e52f93c
Na3im <bruhnaim1@gmail.com>
2026-08-12 02:13:50 +0100

    changing module system

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-import express from express;
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT;
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
+
+const date = new Date();
+console.log('current date and time is : ' + date);
