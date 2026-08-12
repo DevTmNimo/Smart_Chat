ae638f4d5c8988459647ec47143112d085c5add9
Na3im <bruhnaim1@gmail.com>
2026-08-12 02:21:25 +0100

    random shit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-import express from 'express';
+import express from 'express'; 
 const app = express();
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+const PORT = process.env.PORT || 3000;
 const date = new Date();
-console.log('current date and time is : ' + date);
+const onMessage = () => {
+console.log(`If you're seeing this message first of all fuck you and second of all it's ${date}`);
+};
+onMessage();
+
+app.listen (PORT , () => {
+    console.log(`Hello , the server is running on port ${PORT}`);
+})
